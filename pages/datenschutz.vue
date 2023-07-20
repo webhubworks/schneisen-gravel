61e12a1df11c1b7d03026f3ac873d0cf88216e12
Johannes Ahrndt <ahrndt@webhub.de>
2023-07-20 13:59:06 +0200

    Add privacy page

---
+++ pages/datenschutz.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Datenschutz</h1>
+</template>
+
+<script setup>
+useHead({
+  title: 'Datenschutz',
+  meta: [
+    {
+      name: 'robots',
+      content: 'none'
+    }
+  ]
+})
+</script>
