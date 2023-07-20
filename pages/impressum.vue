826b69cb8b48bc72f099948061cb7732f1178302
Johannes Ahrndt <ahrndt@webhub.de>
2023-07-20 13:57:20 +0200

    Add legal

---
+++ pages/impressum.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>Impressum</h1>
+</template>
+
+<script setup>
+useHead({
+  title: 'Impressum',
+  meta: [
+    {
+      name: 'robots',
+      content: 'none'
+    }
+  ]
+})
+</script>
